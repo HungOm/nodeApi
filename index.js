8c6396b15e0c6acf2de0273461fa939166a22044
HungOm <hung.workspace@gmail.com>
2021-12-02 15:54:18 +0800

    check get request

--- index.js
+++ index.js
@@ -143,5 +143,6 @@ var unifiedServer = function(req,res){
 var router = {
   ping:handlers.ping,
   users:handlers.users,
-  tokens:handlers.tokens
+  tokens:handlers.tokens,
+  checks:handlers.checks
 };
